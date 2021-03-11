529f83ae02429afc9e23b5c93809b53560253579
Joe Liu <31641723+Tzuyuisbae@users.noreply.github.com>
2021-03-11 02:32:10 -0500

    updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true,    
+        "es6": true
     },
     "extends": [
         "eslint:recommended",
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
   return (
@@ -22,6 +23,36 @@ function App() {
   );
 }
 
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/ciphertrick"; //connecting to ciphertrick
+const options = {
+    keepAlive: true,
+    keepAliveInitialDelay: 300000,
+    useNewUrlParser: true
+};
+
+mongoose.connect(mongoURI, options);
+
+mongoose.connection.on('connected', ()=>{  
+    console.log('Mongoose default connection open to ' + mongoURI);
+});
+
+// If the connection throws an error
+mongoose.connection.on('error', (err)=>{  
+    console.log('handle mongo errored connections: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', ()=>{  
+    console.log('Mongoose default connection disconnected');
+});
+
+process.on('SIGINT', ()=>{
+    mongoose.connection.close(()=>{
+        console.log('App terminated, closing mongo connections');
+        process.exit(0);
+    });
+});
 
 
 export default App;
