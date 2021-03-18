d81bebaaae17e8e10fa150ad51e86424750d76cb
victguo <victorguo_123@outlook.com>
2021-03-17 23:59:20 -0400

    Updated signup.jsx to actually have styles and use material components

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,15 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import Head from 'next/head';
 import Router from 'next/router';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-const SignupPage = () => {
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUpPage() {
   const [errorMsg, setErrorMsg] = useState('');
-
-  // call whenever user changes (ex. right after signing up successfully)
-//   useEffect(() => {
-//     // redirect to home if user is authenticated
-//     if (user) Router.replace('/');
-//   }, [user]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,50 +42,79 @@ const SignupPage = () => {
       email: e.currentTarget.email.value,
       password: e.currentTarget.password.value,
     };
+
+    if (!body.email) {
+      return setErrorMsg("Please enter a email address");
+    }
+
+    if (!body.password) {
+      return setErrorMsg("Please enter a password");
+    }
     const res = await fetch('/api/signup', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body),
     });
-    if (res.status === 201) {
+    if (res.status === 200) {
       const userObj = await res.json();
+      Router.replace('/');
       // writing our user object to the state
-      console.log(userObj);
+      // console.log(userObj);
     } else {
       setErrorMsg(await res.text());
     }
   };
 
+  const classes = useStyles();
+
   return (
-    <>
-      <Head>
-        <title>Sign up</title>
-      </Head>
-      <div>
-        <h2>Sign up</h2>
-        <form onSubmit={handleSubmit}>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} onSubmit={handleSubmit} noValidate>
           {errorMsg ? <p style={{ color: 'red' }}>{errorMsg}</p> : null}
-          <label htmlFor="email">
-            <input
-              id="email"
-              name="email"
-              type="email"
-              placeholder="Email address"
-            />
-          </label>
-          <label htmlFor="password">
-            <input
-              id="password"
-              name="password"
-              type="password"
-              placeholder="Create a password"
-            />
-          </label>
-          <button type="submit">Sign up</button>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
         </form>
       </div>
-    </>
+    </Container>
   );
-};
-
-export default SignupPage;
+}
