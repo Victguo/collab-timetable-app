6d8104fbbc8be02681a0c50647410f3ae8fc80b3
victguo <victorguo_123@outlook.com>
2021-03-17 23:58:46 -0400

    Readded temp code

--- middleware/mongodb.js
+++ middleware/mongodb.js
@@ -13,3 +13,36 @@ export default async function database(req, res, next) {
   req.db = client.db(MONGODB_DB);
   return next();
 }
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+ let cached = global.mongo
+
+ if (!cached) {
+   cached = global.mongo = { conn: null, promise: null }
+ }
+ 
+ export async function connectToDatabase() {
+   if (cached.conn) {
+     return cached.conn
+   }
+ 
+   if (!cached.promise) {
+     const opts = {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+     }
+ 
+     cached.promise = MongoClient.connect(MONGODB_URI, opts).then((client) => {
+       return {
+         client,
+         db: client.db(MONGODB_DB),
+       }
+     })
+   }
+   cached.conn = await cached.promise
+   return cached.conn
+ }
